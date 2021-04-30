ca46df9c37f2c28f098869a262ab2b6f9a221476
Daemonlau <daemon.lau@gmail.com>
2021-04-30 21:50:10 +0800

    initial commit

---
+++ app/src/main/java/com/liu/guess/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.liu.guess
+
+//import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        R.drawable.pig
+//        R.string.ok
+//        R.color.messageColor
+    }
+}
